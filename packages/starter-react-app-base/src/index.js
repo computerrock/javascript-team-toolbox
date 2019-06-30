011f2c20b486d81283cddf3d50f049ddb722fdde
Ivan Stankovic <ivan.stankovic@webstorm.rs>
2019-06-30 16:21:24 +0200

    [starter] better separate dev from project dependencies

--- packages/starter-react-app-base/src/index.js
+++ packages/starter-react-app-base/src/index.js
@@ -1,8 +1,8 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import 'svgxuse'; // fixes IE10/11 issue when using external SVG
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'svgxuse'; // fixes IE10/11 issue when using external SVG
 import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
