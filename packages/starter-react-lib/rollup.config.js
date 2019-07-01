6c9b04a3b5aad150048a156acffc5dc6d3cd5725
Ivan Stankovic <ivan.stankovic@webstorm.rs>
2019-07-01 07:10:19 +0200

    [starter] update starter lib to babel 7.4 preset & latest build

--- packages/starter-react-lib/rollup.config.js
+++ packages/starter-react-lib/rollup.config.js
@@ -1,22 +1,29 @@
+import {eslint} from 'rollup-plugin-eslint';
+import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import terser from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default [
     // UMD build
     {
         input: 'src/index.js',
+        external: [],
         output: {
             name: 'CRStarterReactLib',
             file: pkg.browser,
             format: 'umd',
         },
-        external: [],
         plugins: [
+            eslint({
+                fix: true,
+            }),
             babel({
                 exclude: ['node_modules/**'],
             }),
+            json(),
             resolve(),
             commonjs(),
         ],
@@ -24,6 +31,7 @@ export default [
     // ES & CJS build
     {
         input: 'src/index.js',
+        external: [],
         output: [
             {
                 file: pkg.main,
@@ -36,13 +44,32 @@ export default [
                 sourceMap: true,
             },
         ],
-        external: [],
         plugins: [
+            eslint({
+                fix: true,
+            }),
             babel({
                 exclude: ['node_modules/**'],
             }),
+            json(),
             resolve(),
             commonjs(),
+            (process.env.NODE_ENV === 'production' && terser({
+                sourceMap: true,
+                terserOptions: {
+                    compress: {
+                        warnings: false,
+                        comparisons: false,
+                    },
+                    mangle: {
+                        safari10: true,
+                    },
+                    output: {
+                        comments: false,
+                        ascii_only: true,
+                    },
+                },
+            })),
         ],
     },
 ];
