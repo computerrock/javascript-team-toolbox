9ce144375e8fc410da08266dec21fc6e32fd2b9b
Nikola Perisic <nikola.perisic@computerrock.com>
2020-01-14 10:29:36 +0100

    [config] update webpack config, move supported browserslist to .browserslistrc file

--- packages/starter-react-app/config/webpack.config.prod.js
+++ packages/starter-react-app/config/webpack.config.prod.js
@@ -132,12 +132,6 @@ module.exports = {
                                     plugins: () => [
                                         require('postcss-flexbugs-fixes'),
                                         autoprefixer({
-                                            browsers: [
-                                                '>1%',
-                                                'last 4 versions',
-                                                'Firefox ESR',
-                                                'not ie < 9',
-                                            ],
                                             flexbox: 'no-2009',
                                             grid: true,
                                         }),
--- packages/testing-react-app/config/webpack.config.dev.js
+++ packages/testing-react-app/config/webpack.config.dev.js
@@ -144,12 +144,6 @@ module.exports = {
                                     plugins: () => [
                                         require('postcss-flexbugs-fixes'),
                                         autoprefixer({
-                                            browsers: [
-                                                '>1%',
-                                                'last 4 versions',
-                                                'Firefox ESR',
-                                                'not ie < 9',
-                                            ],
                                             flexbox: 'no-2009',
                                             grid: true,
                                         }),
@@ -192,12 +186,6 @@ module.exports = {
                                     plugins: () => [
                                         require('postcss-flexbugs-fixes'),
                                         autoprefixer({
-                                            browsers: [
-                                                '>1%',
-                                                'last 4 versions',
-                                                'Firefox ESR',
-                                                'not ie < 9',
-                                            ],
                                             flexbox: 'no-2009',
                                             grid: true,
                                         }),
