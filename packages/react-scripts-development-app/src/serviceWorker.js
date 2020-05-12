ecac0454349084642de60f8ed985a7d7554f8c2b
Ivan Stankovic <ivan.stankovic@webstorm.rs>
2020-05-12 21:25:48 +0200

    autofix eslint code style issues

--- packages/react-scripts-development-app/src/serviceWorker.js
+++ packages/react-scripts-development-app/src/serviceWorker.js
@@ -11,8 +11,8 @@ const isLocalhost = Boolean(
     || window.location.hostname === '[::1]'
     // 127.0.0.0/8 are considered localhost for IPv4.
     || window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
+    ),
 );
 
 export const register = function register(config) {
@@ -38,7 +38,7 @@ export const register = function register(config) {
                 navigator.serviceWorker.ready.then(() => {
                     console.log( // eslint-disable-line no-console
                         'This web app is being served cache-first by a service '
-                        + 'worker. To learn more, visit https://bit.ly/CRA-PWA'
+                        + 'worker. To learn more, visit https://bit.ly/CRA-PWA',
                     );
                 });
             } else {
@@ -66,7 +66,7 @@ const registerValidSW = function registerValidSW(swUrl, config) {
                             // content until all client tabs are closed.
                             console.log( // eslint-disable-line no-console
                                 'New content is available and will be used when all '
-                                + 'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+                                + 'tabs for this page are closed. See https://bit.ly/CRA-PWA.',
                             );
 
                             // Execute callback
@@ -118,7 +118,7 @@ const checkValidServiceWorker = function checkValidServiceWorker(swUrl, config) 
         })
         .catch(() => {
             console.log( // eslint-disable-line no-console
-                'No internet connection found. App is running in offline mode.'
+                'No internet connection found. App is running in offline mode.',
             );
         });
 };
