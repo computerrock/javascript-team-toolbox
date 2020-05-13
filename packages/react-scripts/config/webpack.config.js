87a2efcfb258a00d747b973d2c5a9eef9d85a447
Ivan Stankovic <ivan.stankovic@webstorm.rs>
2020-05-13 19:34:40 +0200

    upgrade terser optimization module

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -132,14 +132,20 @@ module.exports = function (webpackEnv) {
                 new TerserPlugin({
                     sourceMap: shouldUseSourceMap,
                     terserOptions: {
+                        parse: {
+                            ecma: 8,
+                        },
                         compress: {
+                            ecma: 5,
                             warnings: false,
                             comparisons: false,
+                            inline: 2,
                         },
                         mangle: {
                             safari10: true,
                         },
                         output: {
+                            ecma: 5,
                             comments: false,
                             ascii_only: true,
                         },
