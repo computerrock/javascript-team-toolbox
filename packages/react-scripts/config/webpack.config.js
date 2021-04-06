4a89d0b3289efade5dd744deb81fdfd163d8846d
Ivan Stankovic <ivan.stankovic@webstorm.rs>
2021-04-06 21:41:05 +0200

    enable eslint linting for additional module sources; generalize stylelint method

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -19,7 +19,7 @@ const InterpolateHtmlPlugin = require('@computerrock/react-dev-utils/Interpolate
 const WatchMissingNodeModulesPlugin = require('@computerrock/react-dev-utils/WatchMissingNodeModulesPlugin');
 const ModuleScopePlugin = require('@computerrock/react-dev-utils/ModuleScopePlugin');
 const getLocalBEMIdent = require('@computerrock/react-dev-utils/getLocalBEMIdent');
-const getStylelintPaths = require('@computerrock/react-dev-utils/getStylelintPaths');
+const getLintingPaths = require('@computerrock/react-dev-utils/getLintingPaths');
 const getEnvironment = require('./env');
 const paths = require('./paths');
 // @remove-on-eject-begin
@@ -324,6 +324,8 @@ module.exports = function (webpackEnv) {
                 eslintPath: require.resolve('eslint'),
                 resolvePluginsRelativeTo: __dirname,
                 fix: fixESLintErrors,
+                context: paths.appSrc,
+                files: getLintingPaths(paths.appSrc, paths.appSources),
                 // @remove-on-eject-begin
                 ignore: isExtendingESLintConfig,
                 baseConfig: isExtendingESLintConfig ? undefined
@@ -346,7 +348,7 @@ module.exports = function (webpackEnv) {
                 syntax: 'scss',
                 fix: fixStylelintErrors,
                 context: paths.appSrc,
-                files: getStylelintPaths(paths.appSrc, paths.appSources),
+                files: getLintingPaths(paths.appSrc, paths.appSources),
                 // @remove-on-eject-begin
                 configBasedir: isExtendingStylelintConfig
                     ? paths.ownPath : undefined,
