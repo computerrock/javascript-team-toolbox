722a3e78cc6650bd5cd89af5cb037b85069b5c46
Ivan Stankovic <ivan.stankovic@webstorm.rs>
2022-08-23 02:10:37 +0200

    [toolchains] implement Node recommended module resolving for additional module source paths
    (cherry picked from commit dc6c869dab4846f0b96021c0b3881a19b6e29af2)

--- packages/toolchain-react-app/config/getModuleSourcePaths.js
+++ packages/toolchain-react-app/config/getModuleSourcePaths.js
@@ -2,30 +2,43 @@
 
 const path = require('path');
 const fs = require('fs');
-const chalk = require('chalk');
+const resolveFrom = require('resolve-from');
+const findPkg = require('find-pkg');
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+const getBundlerConfigPaths = (bundlerDirectory) => {
+    const sourcePaths = [];
+
+    try {
+        const bundlerConfig = require(path.resolve(bundlerDirectory, 'bundler.config.js'));
+        (bundlerConfig.sourceModules || []).forEach(sourceModule => {
+            try {
+                const resolvedPath = resolveFrom(bundlerDirectory, sourceModule);
+                const modulePkgPath = findPkg.sync(resolvedPath);
+
+                sourcePaths.push(fs.realpathSync(modulePkgPath.replace('/package.json', '/src')));
+
+                const sourceModulePaths = getBundlerConfigPaths(fs.realpathSync(modulePkgPath.replace('/package.json', '')));
+                sourceModulePaths.forEach(sourceModulePath => sourcePaths.push(sourceModulePath));
+            } catch (error) {
+                // no-op
+            }
+        });
+    } catch (error) {
+        // no-op
+    }
+
+    return sourcePaths;
+};
 
 const getModuleSourcePaths = () => {
     const sourcePaths = [
         resolveApp('src'),
     ];
 
-    const bundlerConfig = require(path.resolve(appDirectory, 'bundler.config.js'));
-    (bundlerConfig.sourceModules || []).forEach(sourceModule => {
-        const modulePath = process.cwd() + '/node_modules/' + sourceModule;
-        sourcePaths.push(fs.realpathSync(modulePath + '/src'));
-
-        try {
-            const sourceModuleBundlerConfig = require(fs.realpathSync(modulePath + '/bundler.config.js'));
-
-            (sourceModuleBundlerConfig.sourceModules || []).forEach(sourceModule => {
-                sourcePaths.push(fs.realpathSync(modulePath + '/node_modules/' + sourceModule + '/src'));
-            });
-        } catch (error) {
-            // no-op
-        }
-    });
+    const sourceModulePaths = getBundlerConfigPaths(appDirectory);
+    sourceModulePaths.forEach(sourceModulePath => sourcePaths.push(sourceModulePath));
 
     return [...new Set(sourcePaths)];
 };
