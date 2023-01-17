562fcfa7933fabe687df4dc6654fa5516f7e40ab
Ivan Stankovic <ivan.stankovic@webstorm.rs>
2023-01-17 16:11:21 +0100

    [storybook] implement Node recommended module resolving for additional module source paths
    (cherry picked from commit 176a2c0549108e6f829632f64728bd344545e7c0)

--- packages/storybook-preset-react-app/config/getModuleSourcePaths.js
+++ packages/storybook-preset-react-app/config/getModuleSourcePaths.js
@@ -2,20 +2,45 @@
 
 const path = require('path');
 const fs = require('fs');
-
+const resolveFrom = require('resolve-from');
+const findPkg = require('find-pkg');
 const appDirectory = fs.realpathSync(process.cwd());
-const bundlerConfig = require(path.resolve(appDirectory, 'bundler.config.js'));
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+const getBundlerConfigPaths = (bundlerDirectory) => {
+    const sourcePaths = [];
+
+    try {
+        const bundlerConfig = require(path.resolve(bundlerDirectory, 'bundler.config.js'));
+        (bundlerConfig.sourceModules || []).forEach(sourceModule => {
+            try {
+                const resolvedPath = resolveFrom(bundlerDirectory, sourceModule);
+                const modulePkgPath = findPkg.sync(resolvedPath);
+
+                sourcePaths.push(fs.realpathSync(modulePkgPath.replace('/package.json', '/src')));
+
+                const sourceModulePaths = getBundlerConfigPaths(fs.realpathSync(modulePkgPath.replace('/package.json', '')));
+                sourceModulePaths.forEach(sourceModulePath => sourcePaths.push(sourceModulePath));
+            } catch (error) {
+                // no-op
+            }
+        });
+    } catch (error) {
+        // no-op
+    }
+
+    return sourcePaths;
+};
 
 const getModuleSourcePaths = () => {
     const sourcePaths = [
         resolveApp('src'),
     ];
-    (bundlerConfig.sourceModules || []).forEach(sourceModule => {
-        sourcePaths.push(fs.realpathSync(process.cwd() + '/node_modules/' + sourceModule + '/src'));
-    });
 
-    return sourcePaths;
+    const sourceModulePaths = getBundlerConfigPaths(appDirectory);
+    sourceModulePaths.forEach(sourceModulePath => sourcePaths.push(sourceModulePath));
+
+    return [...new Set(sourcePaths)];
 };
 
 module.exports = getModuleSourcePaths;
